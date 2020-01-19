79ffb68e73770c5fecb6e123190f135a81fb055b
David Mihal <david@dmihal.com>
2020-01-19 00:09:11 -0500

    Updates

---
+++ precache-manifest.728f5aa5148ccbede56ef064f7fd5bb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f9b0d43c680f782bdfe5980c631db2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5c14faa80e162ee3fd8",
+    "url": "/static/css/main.8f93d9eb.chunk.css"
+  },
+  {
+    "revision": "84aa6aa5bbead7e55094",
+    "url": "/static/js/2.279e5132.chunk.js"
+  },
+  {
+    "revision": "99c5655d2432b8199a89e031b7b8d3f7",
+    "url": "/static/js/2.279e5132.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5c14faa80e162ee3fd8",
+    "url": "/static/js/main.080c2b3c.chunk.js"
+  },
+  {
+    "revision": "5c294b3ede6cc00a693f",
+    "url": "/static/js/runtime-main.a305f9cb.js"
+  }
+]);
