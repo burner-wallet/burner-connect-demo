37d08aff0e280cb786aeeeb611f71b5fe16cf134
David Mihal <david@dmihal.com>
2020-01-29 22:30:32 -0500

    Show assets & update account

--- src/WalletInfo.js
+++ src/WalletInfo.js
@@ -3,15 +3,33 @@ import { web3, provider } from './web3';
 
 const WalletInfo = () => {
   const [address, setAddress] = useState(null);
+  const [assets, setAssets] = useState([]);
 
   useEffect(() => {
     web3.eth.getAccounts().then(accounts => setAddress(accounts[0]));
-  }, [])
+    provider.getAssets().then(_assets => setAssets(_assets));
+
+    const changeListener = (accounts) =>setAddress(accounts[0]);
+    provider.on('accountsChanged', changeListener);
+
+    return () => {
+      provider.off(changeListener);
+    };
+  }, []);
 
   return (
     <div>
       <div>Connected to {provider.wallet.name} ({provider.wallet.origin})</div>
       {address && (<div>{address}</div>)}
+
+      {assets.length > 0 && (<div>Assets:</div>)}
+      <ul>
+        {assets.map(asset => (
+          <li key={asset.id}>
+            {asset.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
